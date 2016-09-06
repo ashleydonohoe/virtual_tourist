88af90dffd53b0feb9c1cead851f8cb04e6d5cb5
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-09-05 22:32:49 -0400

    added todos for FlickrAPI

---
+++ VirtualTourist/FlickrAPIClient.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrAPIClient.swift
+//  VirtualTourist
+//
+//  Created by Gabriele on 9/5/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// TODO: Add method to create BBOX
+
+// TODO: Add method to create Flickr URL
+
+// TODO: Add Flickr method for downloading images by location
